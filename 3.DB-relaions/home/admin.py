943cca6b645f99ae3d90b882e08d4cf6c5942f9a
Mizan96 <mizanurcse12@gmail.com>
2024-08-29 17:55:28 +0600

    Admin panel customization

--- 3.DB-relaions/home/admin.py
+++ 3.DB-relaions/home/admin.py
@@ -1,5 +1,7 @@
 from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http import HttpRequest
 from    .models import Car, Driver, Owner, Passenger
 
 
@@ -21,6 +23,23 @@ class CarAdmin(admin.ModelAdmin): # adding search option into admin panel
         # else:
             obj.updated_by = request.user
         obj.save()
+
+    # def get_queryset(self, request):
+    #     qs = super(CarAdmin, self).get_queryset(request)
+    #     if request.user.is_superuser:
+    #         return qs
+    #     return qs.filter(user=request.user)
+    
+    # def has_change_permission(self, request, obj=None):
+    #     if not obj:
+    #         return True
+    #     return obj.user == request.user or request.user.is_superuser
+    
+
+    # def get_readonly_fields(self, request, obj):
+    #     if request.user.is_superuser:
+    #         return []
+    #     return ['user']
 
     
 admin.site.register(Car)
