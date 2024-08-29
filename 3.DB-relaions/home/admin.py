e3cbce988919d5acc31955645bf87968ad066f54
Mizan96 <mizanurcse12@gmail.com>
2024-08-29 16:31:04 +0600

    search in admin panel

--- 3.DB-relaions/home/admin.py
+++ 3.DB-relaions/home/admin.py
@@ -3,7 +3,14 @@ from    .models import Car, Driver, Owner, Passenger
 
 
 # Register your models here.
-admin.site.register(Car)
+
+class CarAdmin(admin.ModelAdmin): # adding search option into admin panel
+    search_fields = ['user__username', 'user__email', 'name']
+    # raw_id_fields = ['user']
+    class Meta:
+        model = Car
+    
+admin.site.register(Car, CarAdmin)
 admin.site.register(Driver)
 admin.site.register(Owner)
 admin.site.register(Passenger)
