cc03bb892c828e285fc6c69a760ba852606da273
Mizan96 <mizanurcse12@gmail.com>
2024-08-29 15:45:16 +0600

    on_delete in ForeignKey

--- 3.DB-relaions/home/admin.py
+++ 3.DB-relaions/home/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from    .models import Car, Driver
+from    .models import Car, Driver, Owner, Passenger
 
 
 # Register your models here.
 admin.site.register(Car)
 admin.site.register(Driver)
+admin.site.register(Owner)
+admin.site.register(Passenger)
--- 3.DB-relaions/home/models.py
+++ 3.DB-relaions/home/models.py
@@ -16,11 +16,41 @@ class Car(models.Model):
     
 class Driver(models.Model): 
     """
-    ForeignKey = ManyToOneField()   
-    Many User have nay car, but a car only have 1 user
+    Many to Many Relationship  
     """
     user    = models.ManyToManyField(User)
     car     = models.CharField(max_length=120)
 
     def __str__(self):
         return self.car
+    
+class Owner(models.Model): 
+    """
+    One to one relationship
+    """
+    owner   = models.OneToOneField(User,on_delete=models.CASCADE)
+    car     = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.car
+    
+
+def set_delete_user():
+    return User.objects.get_or_create(username='deleted')[0] # get_or_create -> (obj, true)
+
+
+
+class Passenger(models.Model):
+    """
+    Data base record delation
+    """
+    # user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    # user = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default=1)
+    # user = models.ForeignKey(User, on_delete=models.CASCADE) # all the records will be deleted with user delation
+    # user = models.ForeignKey(User, on_delete=models.PROTECT) # cannot delete user
+    user = models.ForeignKey(User, on_delete=models.SET(set_delete_user))
+    car     = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.car
+
